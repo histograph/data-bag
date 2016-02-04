c85fda1ed7de8e26f51e7f3a339f9020d133b650
Rein van 't Veer <rein.van.t.veer@geodan.nl>
2016-02-04 17:30:55 +0100

    Test suite with training wheels. Does only buildings so far

--- bag.js
+++ bag.js
@@ -1,19 +1,29 @@
-var fs = require('fs');
-var path = require('path');
-var Promise = require('bluebird');
-var request = require('request');
-var yauzl = require('yauzl');
-var mkdirp = require('mkdirp');
-var highland = require('highland');
+'use strict';
+
+var
+    fs = require('fs'),
+    path = require('path'),
+    Promise = require('bluebird'),
+    request = require('request'),
+    yauzl = require('yauzl'),
+    mkdirp = require('mkdirp'),
+    highland = require('highland'),
+    GJV = require('geojson-validation'),
+    jsts = require('jsts'),
+    proj4 = require('proj4'),
+    reproject = require('reproject');
 
 // The whole XML misery
 var sax = require('sax'),
     strict = true, // set to false for html-mode
     parser = sax.parser(strict);
-var saxpath = require('saxpath');
-var xml2js = require('xml2js');
+var saxpath = require('saxpath'),
+    xml2js = require('xml2js');
 
 var bagconfig = require('./config.json');
+
+var geometryFactory = new jsts.geom.GeometryFactory();
+var reader = new jsts.io.GeoJSONReader();
 
 // ==================================== API ====================================
 
@@ -24,8 +34,12 @@ module.exports = {
     downloadDataFile : downloadDataFile,
     extractZipfile : extractZipfile,
     listBuildingFiles : listBuildingFiles,
+    extractBuildingsFromDir : extractBuildingsFromDir,
     extractBuildingsFromFile : extractBuildingsFromFile,
+    validateCoords : validateCoords,
     joinGMLposlist : joinGMLposlist,
+    toWGS84 : toWGS84,
+    write : write,
     steps : [
         download,
         convert
@@ -108,82 +122,159 @@ function extractZipfile(zipfilename, extractdir, callback) {
 }
 
 
+function extractBuildingsFromDir(dir, targetFile){
+    return new Promise(function(resolve, reject){
+        var filesToProcess = listBuildingFiles(dir);
+
+        var fileListStream = highland(filesToProcess)
+        fileListStream.each(file => {
+            console.log("Processing file", file);
+            extractBuildingsFromFile(file, targetFile)
+                .then(buildings => {
+                    highland(buildings).each(function (building) {
+                        fs.appendFileSync(targetFile, JSON.stringify(building) + "\n");
+                    });
+                });
+        });
+
+        fileListStream.done( () => resolve(true));
+    });
+}
+
+
 function listBuildingFiles(dir){
-    return fs.readdirSync(dir).filter(function(file){
+    return fs.readdirSync(dir).filter(file => {
         return file.slice(-4) !== ".zip" && file.search("PND") > 0;
+    }).map(file => path.join(dir, file))
+}
+
+
+function extractBuildingsFromFile(file, targetFile){
+    return new Promise( (resolve, reject) => {
+        var buildings = [];
+        var parser = new xml2js.Parser();
+
+        var saxStream = sax.createStream(strict);
+        fs.createReadStream(file, { encoding: 'utf8' })
+            .pipe(saxStream);
+
+        var streamer   = new saxpath.SaXPath(saxStream, '//bag_LVC:Pand');
+
+        streamer.on('match', xml => {
+            parser.parseString(xml, (err, result) => {
+                if (!err) {
+                    joinGMLposlist(result["bag_LVC:Pand"]["bag_LVC:pandGeometrie"][0]["gml:Polygon"][0]["gml:exterior"][0]["gml:LinearRing"][0]["gml:posList"][0]["_"])
+                        .then(list => {
+                            var polygon = [];
+                            polygon[0] = list;
+
+                            buildings.push({
+                                uri: module.exports.url + '/pand/' + result["bag_LVC:Pand"]["bag_LVC:identificatie"][0],
+                                id: result["bag_LVC:Pand"]["bag_LVC:identificatie"][0],
+                                bouwjaar: result["bag_LVC:Pand"]["bag_LVC:bouwjaar"][0],
+                                geometry: {
+                                    type: "Polygon",
+                                    coordinates: polygon
+                                }
+                            })
+                        });
+                } else {
+                    console.error("Error parsing xml element", xml, "\n", err);
+                }
+            });
+        });
+
+        saxStream.on("error", function (err) {
+            console.error("saxStream threw error", err.stack);
+            // clear the error
+            this._parser.error = null;
+            this._parser.resume()
+        });
+        saxStream.on('end', function(){
+            resolve(buildings);
+        });
+
     })
 }
 
-function extractBuildingsFromDir(dir, targetFile){
-    return new Promise(function(resolve, reject){
-        filesToProcess = listBuildingFiles(dir);
-        highland(filesToProcess).map(function(file) {
-            extractBuildingsFromFile(file, targetFile)
-                .then(function (buildings) {
-                    highland(buildings).each(function (building) {
-                        //append to form ndjson
-                        fs.appendFileSync(targetFile, JSON.stringify(building));
-                        resolve(true);
-                    });
-                });
-        })
-        .series();
+function joinGMLposlist(posList, type) {
+    return new Promise( (resolve, reject) => {
+        posList = posList.split(' ');
+
+        var geojsonPosList = [];
+        var counter = 0;
+
+        while (posList.length !== 0) {
+            var point = [];
+            point.push(parseFloat(posList.shift()));
+            point.push(parseFloat(posList.shift()));
+            posList.shift(); // skip 3d height
+            point = toWGS84(point)
+                .map(coordinate => parseFloat(
+                    coordinate.toFixed(7))
+                );
+            geojsonPosList[counter] = point;
+
+            counter += 1;
+        }
+
+        if (type === "Polygon") {
+            //add extra level of array
+            var extralevel = [[[]]];
+            extralevel[0] = geojsonPosList;
+            resolve(extralevel);
+
+        }
+        resolve(geojsonPosList);
+
     });
 }
 
-function extractBuildingsFromFile(file, targetFile, callback){
-    var buildings = [];
-    var parser = new xml2js.Parser();
+function validateCoords(geojsoncoords, type) {
+    return new Promise( (resolve, reject) => {
+        var geojson = {
+            type: "Feature",
+            geometry: {
+                type: type,
+                coordinates: geojsoncoords
+            },
+            properties: {}
+        };
 
-    var saxStream = sax.createStream(strict);
-    fs.createReadStream(file, { encoding: 'utf8' })
-        .pipe(saxStream);
-
-    var streamer   = new saxpath.SaXPath(saxStream, '//bag_LVC:Pand');
-
-    streamer.on('match', function(xml) {
-        parser.parseString(xml, function (err, result) {
-            if (!err) {
-                buildings.push({
-                    uri: module.exports.url + '/pand/' + result["bag_LVC:Pand"]["bag_LVC:identificatie"][0],
-                    id: result["bag_LVC:Pand"]["bag_LVC:identificatie"][0],
-                    bouwjaar: result["bag_LVC:Pand"]["bag_LVC:bouwjaar"][0],
-                    geometrie: joinGMLposlist(result["bag_LVC:Pand"]["bag_LVC:pandGeometrie"][0]["gml:Polygon"][0]["gml:exterior"][0]["gml:LinearRing"][0]["gml:posList"][0]["_"])
-                });
+        GJV.isFeature(geojson, (valid, errs) => {
+            if (!valid) {
+                console.error("Validator rejecting geometry due to:", errs);
+                reject(errs);
             } else {
-                console.error("Error parsing xml element", xml, "\n", err);
+                console.log("JSTS evaluated feature as valid: " + isValidGeoJSON(geojson) + "\n");
+                resolve(isValidGeoJSON(geojson));
             }
         });
     });
-
-    saxStream.on("error", function (e) {
-        // unhandled errors will throw, since this is a proper node event emitter.
-        console.error("error!", e);
-        // clear the error
-        this._parser.error = null;
-        this._parser.resume()
-    });
-    saxStream.on('end', function(){
-        return callback(buildings);
-    });
-
 }
 
-function joinGMLposlist(posList) {
-    posList = posList.split(' ');
+function isValidGeoJSON(geoJSONPolygon){
+    var jstsGeometry  = reader.read(geoJSONPolygon.geometry);
 
-    var geojsonPosList = [];
-    var counter = 0;
-
-    while (posList.length !== 0) {
-        geojsonPosList[counter] = [];
-        geojsonPosList[counter].push(posList.shift());
-        geojsonPosList[counter].push(posList.shift());
-        geojsonPosList[counter].push(posList.shift());
-        counter += 1;
+    if (jstsGeometry) {
+        var validator = new jsts.operation.valid.IsValidOp(jstsGeometry);
+        return validator.isValid();
     }
-
-    return geojsonPosList;
 }
+
+function write(data, file) {
+    console.log("Wrote some data\n");
+    fs.appendFileSync(file, JSON.stringify(data) + "\n");
+}
+
+function toWGS84(point) {
+    var EPSG28992 = "+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +towgs84=565.417,50.3319,465.552,-0.398957,0.343988,-1.8774,4.0725 +units=m +no_defs";
+
+    return proj4(
+        EPSG28992,
+        proj4("WGS84")
+    ).forward(point);
+}
+
 function convert(config, dir, writer, callback) {
 }
